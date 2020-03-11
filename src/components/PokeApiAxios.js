2fcdca2a90b03567c9d0ed3d508fff74b8314f2c
Tyler <taehyun9202@hotmail.com>
2020-03-11 17:14:43 -0400

    second commit

--- src/components/PokeApiAxios.js
+++ src/components/PokeApiAxios.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const PokeApiAxios = () => {
+    const [pokemon, setPokemon] = useState([]);
+    const getPoke = e => {
     axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
-        .then(response=>console.log(response))
+        .then(response=>setPokemon(response.data.results))
         .catch(error => console.log(error));
-    
+    }
     return (
         <div>
-            <button type="submit" onClick={PokeApiAxios}>Fetch Pokemon!</button>
+            <button type="submit" onClick={getPoke}>Fetch Pokemon!</button>
+            {pokemon.map((pokemon, i)=>
+                <li key={i}>{pokemon.name}</li>
+            )}
         </div>
     );
 }
